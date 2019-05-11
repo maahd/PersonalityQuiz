577010ef5a0307857df0a8ea63fef43922ce417d
maahd <maahdsh@gmail.com>
2019-05-11 20:49:41 +0200

    determine the most frequent answers

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -23,6 +23,12 @@ class ResultsViewController: UIViewController {
         for response in responseTypes {
             frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
         }
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        { (pair1, pair2) -> Bool in
+            return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswers = frequentAnswersSorted.first!.key
     }
     
 
